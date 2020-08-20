1fbbdbc37af8013ad93e49c838d6b3e7f5033524
arashsingh5 <62629198+arashsingh5@users.noreply.github.com>
2020-08-20 11:54:11 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,16 @@
+/** hello.java 
+* Java Program Name
+* Arashpreet Singh 
+* 08/20/20
+* CMCS 255 Section 01
+*/
+public class hello {
+	public static void main (String[] args) {
+	System.out.println("Arashpreet Singh");
+	System.out.println("Lab 1");
+	System.out.println("First Lab Project");
+	System.out.println("Aug 20th");
+	System.out.println("CMCS 255 and section 01");
+	System.out.println("");
+	}
+}
